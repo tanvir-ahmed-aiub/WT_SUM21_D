efa20190620d9989bb97fde3fa62f5208ba62bdc
taanveerahmeed <taanveerahmeed@gmail.com>
2021-07-17 10:21:09 +0600

    Css

--- MVC/models/db_config.php
+++ MVC/models/db_config.php
@@ -9,6 +9,7 @@
 	function execute($query){   //responsible for running insert,update,delete
 		global $db_server,$db_uname,$db_pass,$db_name;
 		$conn = mysqli_connect($db_server,$db_uname,$db_pass,$db_name);
+		$query = mysqli_real_escape_string($conn, $query);
 		if($conn){
 			if(mysqli_query($conn,$query)){
 				return true;
---
+++ css/index.php
@@ -0,0 +1,77 @@
+<html>
+	<head>
+		<style>
+			p{
+				text-align:justify;
+			}
+			#p1{
+				/*color:red;
+				background-color:black;
+				font-family:consolas;
+				font-style:italic;*/
+				/*margin-top:50px;
+				margin-left:400px;
+				margin-right:100px;
+				margin-bottom:200px;*/
+				margin:20px;
+				color:yellow;
+				background-color:black;
+				font-family:verdana;
+				
+			}
+			#p10{
+				color:#01EFeF;
+				background-color:rgb(0,0,0);
+				font-family:calibri;
+				font-style:italic;
+				
+			}
+			.bd{
+				color:yellow;
+				background-color:black;
+				font-family:verdana;
+				padding:20px;
+				margin:10px;
+			}
+			.btn-link{
+				background-color:rgb(73,127,175);
+				font-family:calibri;
+				color:white;
+				padding-left:20px;
+				padding-right:20px;
+				padding-top:10px;
+				padding-bottom:10px;
+				text-decoration:none;
+				border-radius:3px;
+			}
+			.btn-link:hover{
+				background-color:rgb(0,78,162);
+			}
+			.btn-link:active{
+				background-color:red;
+			}
+		</style>
+	</head>
+	<body>
+		<br><br>
+		<div>
+			<a  class="btn-link" href="">Link1</a>
+			<a class="btn-link" href="">Link2</a>
+			<a class="btn-link" href="">Link4</a>
+			<a class="btn-link" href="">Link3</a>
+			
+		</div>
+		<p id="p1">Video provides a powerful way to help you prove your point. When you click Online Video, you can paste in the embed code for the video you want to add. You can also type a keyword to search online for the video that best fits your document.</p>
+		<p class="bd">To make your document look professionally produced, Word provides header, footer, cover page, and text box designs that complement each other. For example, you can add a matching cover page, header, and sidebar. Click Insert and then choose the elements you want from the different galleries.</p>
+		<p class="">Themes and styles also help keep your document coordinated. When you click Design and choose a new Theme, the pictures, charts, and SmartArt graphics change to match your new theme. When you apply styles, your headings change to match the new theme.</p>
+		<p>Save time in Word with new buttons that show up where you need them. To change the way a picture fits in your document, click it and a button for layout options appears next to it. When you work on a table, click where you want to add a row or a column, and then click the plus sign.</p>
+		<p>Reading is easier, too, in the new Reading view. You can collapse parts of the document and focus on the text you want. If you need to stop reading before you reach the end, Word remembers where you left off - even on another device.</p>
+		<p class="bd">Video provides a powerful way to help you prove your point. When you click Online Video, you can paste in the embed code for the video you want to add. You can also type a keyword to search online for the video that best fits your document.</p>
+		<p>To make your document look professionally produced, Word provides header, footer, cover page, and text box designs that complement each other. For example, you can add a matching cover page, header, and sidebar. Click Insert and then choose the elements you want from the different galleries.</p>
+		<p>Themes and styles also help keep your document coordinated. When you click Design and choose a new Theme, the pictures, charts, and SmartArt graphics change to match your new theme. When you apply styles, your headings change to match the new theme.</p>
+		<p>Save time in Word with new buttons that show up where you need them. To change the way a picture fits in your document, click it and a button for layout options appears next to it. When you work on a table, click where you want to add a row or a column, and then click the plus sign.</p>
+		<p id="p10">Reading is easier, too, in the new Reading view. You can collapse parts of the document and focus on the text you want. If you need to stop reading before you reach the end, Word remembers where you left off - even on another device.</p>
+
+
+	</body>
+</html>
